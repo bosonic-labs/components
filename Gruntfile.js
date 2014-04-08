5b9fb33e934dd4e22a02a95bfd823ae2b9debe50
Raphaël Rougeron <goldoraf@gmail.com>
2014-04-08 16:49:46 +0200

    New release includes tooltip element

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,7 +9,8 @@ module.exports = function(grunt) {
     'node_modules/b-datepicker/src/*.html',
     'node_modules/b-sortable/src/*.html',
     'node_modules/b-collapsible/src/*.html',
-    'node_modules/b-accordion/src/*.html'
+    'node_modules/b-accordion/src/*.html',
+    'node_modules/b-tooltip/src/*.html'
   ];
 
   grunt.initConfig({
@@ -23,6 +24,17 @@ module.exports = function(grunt) {
         src: elements,
         css: 'dist/bosonic-components.css',
         js:  'dist/bosonic-components.js'
+      }
+    },
+
+    uglify: {
+      options: {
+        mangle: false
+      },
+      components: {
+        files: {
+          'dist/bosonic-components.min.js': ['dist/bosonic-components.js']
+        }
       }
     },
 
@@ -63,12 +75,13 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-bosonic');
   grunt.loadNpmTasks('grunt-karma');
 
   grunt.registerTask('default', ['dist']);
   grunt.registerTask('test', ['dist', 'copy:tests', 'karma']);
   grunt.registerTask('demo', ['dist', 'copy:demo', 'connect']);
-  grunt.registerTask('dist', ['clean', 'bosonic']);
+  grunt.registerTask('dist', ['clean', 'bosonic', 'uglify']);
 
 };
